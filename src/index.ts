7a0ad445bc3e70a9769520fb16113a775f4ff28b
marcos8896 <elyomarcos@gmail.com>
2019-05-22 20:34:43 -0500

    Add basic size validation for incoming files (images) - #19

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, Options } from 'graphql-yoga';
 import { prisma } from './generated/prisma-client';
 import resolvers from './resolvers';
 import db from './db-prisma-binding';
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context: request => ({
-    ...request,
-    prisma,
-    db,
-  }),
-  resolverValidationOptions: {
-    requireResolversForResolveType: false,
-  },
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: (request): any => ({
+        ...request,
+        prisma,
+        db,
+    }),
+    resolverValidationOptions: { requireResolversForResolveType: false },
 });
 
-const options = {
-  port: process.env.GRAPHQL_SERVER_PORT,
+const maxMBs = 1;
+const options: Options = {
+    port: process.env.GRAPHQL_SERVER_PORT,
+    cors: { origin: 'http://localhost:3000', credentials: true },
+    uploads: {
+        maxFileSize: maxMBs * 1024 * 1024,
+        maxFiles: 1,
+    },
 };
 
-server.start(options, ({ port }) => console.log(`Server is running on http://localhost:${port}`));
+server.start(options, ({ port }): void => console.log(`Server is running on http://localhost:${port}`));
