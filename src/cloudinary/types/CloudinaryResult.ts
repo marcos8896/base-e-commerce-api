f3f9d1b30a848dda5efce705cf45bb552705beb0
marcos8896 <elyomarcos@gmail.com>
2019-06-03 21:39:03 -0500

    Fix #19 - Add main structure to register Items with images

---
+++ src/cloudinary/types/CloudinaryResult.ts
@@ -0,0 +1,20 @@
+export default interface CloudinaryResult {
+    public_id: string;
+    version: number;
+    signature: string;
+    width: number;
+    height: number;
+    format: string;
+    resource_type: string;
+    created_at: string;
+    tags: string[];
+    bytes: number;
+    type: string;
+    etag: string;
+    placeholder: boolean;
+    url: string;
+    secure_url: string;
+    access_mode: string;
+    original_filename: string;
+    eager: any[];
+}
--- src/resolvers/Mutation/item.ts
+++ src/resolvers/Mutation/item.ts
@@ -1,11 +1,35 @@
 import { Context } from '../../utils';
 import { Item } from '../../generated/prisma-client/index';
+import { cloudinaryImageUpload } from '../../cloudinary/cloudinaryImageUpload';
+interface ImageFile {
+    filename: string;
+    mimetype: string;
+    encoding: string;
+    createReadStream: any;
+}
+
+interface ExtendedItem {
+    imageFile: ImageFile;
+}
+
 export const item = {
-    async createItem(parent, args: any, ctx: Context): Promise<Item> {
-        // TODO: Check if current user is logged in
-        const item = await ctx.prisma.createItem({
-            ...args,
-        });
-        return item;
+    async createItem(parent, args: ExtendedItem & Item, ctx: Context): Promise<Item> {
+        try {
+            // TODO: Check if current user is logged in
+            const image = await args.imageFile;
+            const imageStream = image.createReadStream();
+            const cloudinaryResult = await cloudinaryImageUpload({ stream: imageStream });
+            const item = await ctx.prisma.createItem({
+                description: args.description,
+                title: args.title,
+                price: args.price,
+                image: cloudinaryResult.secure_url,
+                largeImage: cloudinaryResult.eager[0].secure_url,
+            });
+            return item;
+        } catch (err) {
+            console.log('err: ', err);
+            throw new Error(`Failed to register item! - Err: ${err.message}`);
+        }
     },
 };
